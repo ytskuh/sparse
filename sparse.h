e8fb530548ad60ed4676f0dea764dce5cb2f3060
Yutsing Kuh <yutsing@outlook.com>
2023-10-28 09:13:46 +0800

    Move template function to .h

--- sparse.h
+++ sparse.h
@@ -31,6 +31,40 @@ public:
     void print_() const;
 };
 
+#include <cstddef>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+using std::vector;
+using std::size_t;
+using std::fill;
+using std::cout;
+using std::endl;
+
+template <typename T>
+T& DiaMatrix<T>::operator()(size_t i, size_t j) {
+    if(i >= rows_ || j >= cols_) {
+        throw std::out_of_range("DiaMatrix::operator(): index out of range");
+    }
+    for(size_t k = 0; k < offsets.size(); ++k) {
+        if(offsets[k] == i-j) {
+            return data_[k][j];
+        }
+    }
+    return T{};
+}
+
+template <typename T>
+void DiaMatrix<T>::print_() const {
+    for(size_t i = 0; i < rows_; ++i) {
+        for(size_t j = 0; j < cols_; ++j) {
+            cout<<(*this)(i, j)<<" ";
+        }
+        cout<<endl;
+    }
+}
+
 template <typename U>
 DiaMatrix<U> diags(const vector<U>& diagonals, const vector<int>& offsets, size_t rows, size_t cols, bool wrap = false) {
     DiaMatrix<U> result;
